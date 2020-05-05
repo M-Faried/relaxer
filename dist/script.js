65d408298d2bd86d12dcbe778039af4f38ef6535
M Faried <m.a.faried@gmail.com>
2020-05-05 02:28:40 +0200

    Added the implementation

---
+++ dist/script.js
@@ -0,0 +1,45 @@
+const container = document.getElementById('container');
+const text = document.getElementById('circle-text');
+const playVoiceCheck = document.getElementById('play-voice');
+
+const timeBreatheIN = 4000;
+const timeHold = 7000;
+const timeBreatheOut = 8000;
+const utterance = new SpeechSynthesisUtterance();
+utterance.rate = 0.6;
+
+let _playVoice = false;
+
+playVoiceCheck.addEventListener(
+  'click',
+  () => (_playVoice = playVoiceCheck.checked)
+);
+
+breatheIN();
+
+function breatheIN() {
+  text.innerText = 'Breathe In!';
+  sayMessage('breathe in');
+  container.className = 'container grow';
+  setTimeout(hold, timeBreatheIN);
+}
+
+function hold() {
+  text.innerText = 'Hold!';
+  sayMessage('hold');
+  setTimeout(breatheOut, timeHold);
+}
+
+function breatheOut() {
+  text.innerText = 'Breathe Out!';
+  sayMessage('breathe out');
+  container.className = 'container shrink';
+  setTimeout(breatheIN, timeBreatheOut);
+}
+
+function sayMessage(message) {
+  if (_playVoice) {
+    utterance.text = message;
+    speechSynthesis.speak(utterance);
+  }
+}
